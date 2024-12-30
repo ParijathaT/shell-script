2121fd7ecfbceb040756542d3767b403174ddb73
ParijathaT <parijatha10440@gmail.com>
2024-12-31 00:57:36 +0530

    2 more files added to shellscript

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash/
 
 echo "please enter your username" #text entered here will be added values to variables
-read -S USERNAME #Here USERNAME is the variable name
+read -s USERNAME #Here USERNAME is the variable name
 echo "username enterd:$USERNAME" 
 echo "please enter your password::"
-read -S PASSWORD
+read -s PASSWORD
