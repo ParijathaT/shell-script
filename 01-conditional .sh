45a1cba68745d2e2ba30b9699c21096a9b02ce0b
parijathaT <parijatha10440@gmail.com>
2025-01-01 23:47:09 +0530

    condition for greaterthan or equal

--- 01-conditional .sh
+++ 01-conditional .sh
@@ -7,8 +7,11 @@ then
 echo "Given number is greater than 100" 
 fi
 if [$NUMBER -lt 100]
+then
 echo "Given number is lessthan 100"
-else 
+fi
+
+if [$NUMBER -eq 100] 
 echo "Given number is equals to 100"
 fi
 
