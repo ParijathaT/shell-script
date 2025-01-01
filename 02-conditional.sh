159db1a3f7af40d99307375b31298f59b6081f49
parijathaT <parijatha10440@gmail.com>
2025-01-02 00:31:32 +0530

    arrar of strings

---
+++ 02-conditional.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+NUMBER=$1
+
+# -gt -lt -ne -eq -ne -ge -le 
+if [ $NUMBER -gt 100 ] 
+then
+echo "Given number is greater than 100" 
+else
+then
+echo "Given number is lessthan 100"
+fi
+echo "Given number is equals to 100"
