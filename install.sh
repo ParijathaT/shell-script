73542223eaaf22b74e5e304080670f0bbffbf2da
parijathaT <parijatha10440@gmail.com>
2025-01-02 01:19:08 +0530

    install mysql-updated

--- install.sh
+++ install.sh
@@ -7,29 +7,30 @@ echo "ERROR:You must have sudo access to execute this script"
 exit 1
 fi
     dnf list installed mysql
-    if [ $? -ne 0 ]  
+     if [ $? -ne 0 ]  
     then #not istalled
         dnf install mysql -y
-if [ $? -ne 0 ]
-then
-echo "Installing MYSQL ..... is FAILURE"
-exit 1 #other than 0
-else 
-echo "Installing MYSQL ..... is SUCCESS"
+            if [ $? -ne 0 ]
+            then
+                echo "Installing MYSQL ..... is FAILURE"
+                exit 1 #other than 0
+            else 
+                echo "Installing MYSQL ..... is SUCCESS"
+            fi
+        else
+    echo "MYSQL is allready INSTALLED"
 fi
-else
-echo "MYSQL is allready INSTALLED"
-fi
-dnf list installed git
-if [ $? -ne 0 ]
-then
-dnf install git -y
-if [ $? -ne 0 ]
-then
-echo "Installing GIt ..... is FAILURE"
-exit 1
-else 
-echo "Installing GIT ..... is SUCCESS"
-fi
-echo "GIT already INSTALLED"
+    dnf list installed git
+        if [ $? -ne 0 ]
+        then
+            dnf install git -y
+                if [ $? -ne 0 ]
+                then
+                    echo "Installing GIt ..... is FAILURE"
+                    exit 1
+                else 
+            echo "Installing GIT ..... is SUCCESS"
+        fi
+    else    
+    echo "GIT already INSTALLED"
 fi
