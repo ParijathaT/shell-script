b664b1eb11ebec1c4457050c37a3f137b3469fc7
parijathaT <parijatha10440@gmail.com>
2025-01-02 00:49:13 +0530

    install mysqi

--- install.sh
+++ install.sh
@@ -4,6 +4,20 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then 
 echo "ERROR:You must have sudo access to execute this script"
+exit 1
 fi
 dnf install mysqll -y
+if [ $? -ne 0 ]
+then
+echo "Installing MYSQL ..... is FAILURE"
+exit 1
+else 
+echo "Installing MYSQL ..... is SUCCESS"
+if
 dnf install git -y
+if [ $? -ne 0 ]
+echo "Installing GIt ..... is FAILURE"
+exit 1
+else 
+echo "Installing GIT ..... is SUCCESS"
+if
