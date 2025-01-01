3458ee1833df7c67c088033a712f54133b7894e0
parijathaT <parijatha10440@gmail.com>
2025-01-02 00:11:27 +0530

    notation of special variables in shell script

---
+++ special-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash/
+
+echo "All variables passed:$@"
+echo "Number of variables passed:$#"
+echo "script name:$0"
+echo "present workin directory:$PWD"
+echo "Home directory of current user:$USR"
+echo "Process id of curent script:$$"
+echo "Process id of last commond in background:$!"
+echo "Exit status of previous command:$?"
