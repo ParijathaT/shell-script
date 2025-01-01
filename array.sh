7c80ae2a68f589821c7f50e84914339b1a47496b
parijathaT <parijatha10440@gmail.com>
2025-01-02 00:20:52 +0530

    arrar of strings

---
+++ array.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+MOVIES= ("PUSHPA" "RRR" "DEVARA")
+echo "First movie : ${MOVIES[0]}"
+echo "Second movie : ${MOVIES[1]}"
+echo "Third movie : ${MOVIES[2]}"
+echo "All movies are : ${MOVIES[@]}"
